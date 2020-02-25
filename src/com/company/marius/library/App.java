ac5c45c0135b48202998959a4e3afde0cbfe7b4e
Marius Sabaliauskas <msabaliauskas@gmail.com>
2020-02-25 14:59:25 +0100

    initial menu to users

---
+++ src/com/company/marius/library/App.java
@@ -0,0 +1,51 @@
+package com.company.marius.library;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String choice;
+        do {
+            String initialMenuOptions = "Press: \n"
+                    + "\t- 1: librarian menu\n"
+                    + "\t- 2: user menu\n"
+                    + "\t- q: quit the program\n";
+            System.out.println(initialMenuOptions);
+            choice = sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    handleLibrarian(sc);
+                    break;
+                case "2":
+                    handleUser(sc);
+                    break;
+                case "q":
+                    System.out.println("bye bye!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("incorrect value entered !!");
+                    System.out.println("repeat pleace !!");
+
+
+            }
+
+        }while (true);
+    }
+
+    private static void handleUser(Scanner sc) {
+
+
+    }
+
+    private static void handleLibrarian(Scanner sc) {
+
+
+    }
+
+
+}
