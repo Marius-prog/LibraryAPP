8b5a4c059680ae7e2d9cecfbbf6f1cec6a251c7e
Marius Sabaliauskas <msabaliauskas@gmail.com>
2020-02-25 19:41:46 +0100

    some librarian methods

--- src/com/company/marius/library/App.java
+++ src/com/company/marius/library/App.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class App {
 
+    private static Library lib = new Library();
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -30,40 +32,56 @@ public class App {
                 default:
                     System.out.println("incorrect value entered !!");
                     System.out.println("repeat pleace !!");
-
-
             }
-
         } while (true);
     }
 
-
     private static void handleLibrarian(Scanner sc) {
-
         String choice;
         do {
             System.out.println("\nPress: \n"
                     + "\t- 1: users menu\n"
                     + "\t- 2: books menu");
             choice = sc.nextLine();
-        }while (!choice.equals("1") && !choice.equals("2"));
-
-        if(choice.equals("1")) librarianUsersMenu(sc);
+        } while (!choice.equals("1") && !choice.equals("2"));
+        if (choice.equals("1")) librarianUsersMenu(sc);
         else librariansBooksMenu(sc);
+    }
+
+    private static void librarianUsersMenu(Scanner sc) {
+
+        String choice;
+        do {
+            System.out.println("\nPress: \n"
+                    + "\t- 1: add user\n"
+                    + "\t- 2: list all users\n"
+                    + "\t- 3: remove user");
+            choice = sc.nextLine();
+        } while(!choice.equals("1") && !choice.equals("2") && !choice.equals("3"));
+
+        if(choice.equals("1"))
+            addUser(sc);
+        else if(choice.equals("2"))
+            lib.printAllUsers();
+        else
+            deleteUser(sc);
+    }
+
+    private static void deleteUser(Scanner sc) {
+
+    }
+
+    private static void addUser(Scanner sc) {
+
     }
 
     private static void librariansBooksMenu(Scanner sc) {
 
     }
 
-    private static void librarianUsersMenu(Scanner sc) {
 
-    }
 
     private static void handleUser(Scanner sc) {
 
-
     }
-
-
 }
---
+++ src/com/company/marius/library/Library.java
@@ -0,0 +1,19 @@
+package com.company.marius.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private List<User> users = new ArrayList<>();
+    private List<Book> books = new ArrayList<>();
+    private String usersFileName = "users.ser";
+
+
+    public Library() {
+    }
+
+    public void printAllUsers() {
+
+    }
+}
