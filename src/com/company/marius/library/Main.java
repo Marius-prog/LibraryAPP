e264e650d4ad0a4b2db75c6476480efb644e5cb4
Marius Sabaliauskas <msabaliauskas@gmail.com>
2020-02-25 14:27:53 +0100

    first: test

--- src/com/company/marius/library/Main.java
+++ src/com/company/marius/library/Main.java
@@ -1,8 +1,10 @@
 package com.company.marius.library;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person p = new Person();
     }
 }
---
+++ src/com/company/marius/library/Person.java
@@ -0,0 +1,4 @@
+package com.company.marius.library;
+
+public class Person {
+}
